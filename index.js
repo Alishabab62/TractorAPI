bf7b8e4111d36382d3045d28794d8a78ca54d8bf
alishabab62 <alishabab62@gmail.com>
2023-03-12 17:13:43 +0530

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
 const server = http.createServer(app);
-server.listen(3000,console.log("success"));
+server.listen(3000, console.log("success"));
