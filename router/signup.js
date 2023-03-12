1f139e453b99e9f9ce2b47e6cb28cf9716a171f0
alishabab62 <alishabab62@gmail.com>
2023-03-12 20:56:12 +0530

    api complete

--- router/signup.js
+++ router/signup.js
@@ -1,40 +1,68 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-const SignUp = require("../modals/signUp");
+const User = require("../modals/signUp");
 const bcrypt = require("bcrypt"); 
 
 router.post("/signup", async (req, res) => {
   try {
-    const user = await SignUp.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ message: "user already exists" });
-    } else {
-      const user = new SignUp(req.body);
-      console.log(user);
-      await user
-        .save()
-        .then((result) => {
-          res.status(200).json({ user: result });
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+    const { fname, mname, lname, email, phone, dob, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({ message: "Email is already registered" });
     }
+
+    // Create a new user
+    const user = new User({
+      fname,
+      mname,
+      lname,
+      email,
+      phone,
+      dob,
+      password,
+    });
+
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
-router.get("/login/:email", async (req, res) => {
-  await SignUp.find({ email: req.params.email })
-    .then((user) => {
-      res.status(200).json({ user: user });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user with email exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+    // Check if password is correct
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+    // Return user information
+    res
+      .status(200)
+      .json({ user: { id: user._id, name: user.name, email: user.email } });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
 });
 
 module.exports = router;
