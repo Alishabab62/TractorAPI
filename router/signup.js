113542f949d2f93e5ea53318843a8ae44bb88f1a
alishabab62 <alishabab62@gmail.com>
2023-03-12 17:29:17 +0530

    c

--- router/signup.js
+++ router/signup.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const SignUp = require("../modals/signUp");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); 
 
 router.post("/signup", async (req, res) => {
   try {
